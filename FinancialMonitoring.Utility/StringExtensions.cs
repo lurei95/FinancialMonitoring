9d0d93d9d4703943074e2c2174964b246be2601d
Lukas Reinhardt <ABA101010111010@gmail.com>
2020-09-20 23:05:13 +0200

    - added UsersControllöers with authentication logic - added Utitlity project

---
+++ FinancialMonitoring.Utility/StringExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace FinancialMonitoring.Utility
+{
+  /// <summary>
+  /// Class containing exrtension methods for string
+  /// </summary>
+  public static class StringExtensions
+  {
+    /// <summary>
+    /// Returns a formated string
+    /// </summary>
+    /// <param name="text">The string to format</param>
+    /// <param name="parameters">The parameters with which the string should be formated</param>
+    /// <returns>The formated string</returns>
+    public static string FormatWith(this string text, params object[] parameters)
+      => String.Format(CultureInfo.InvariantCulture, text, parameters);
+
+    /// <summary>
+    /// Encrypts a string
+    /// </summary>
+    /// <param name="text">the text to encrypt</param>
+    /// <returns>The encrypted text</returns>
+    public static string Encrypt(this string text)
+    {
+      MD5 provider = MD5.Create();
+      string salt = "S0m3R@nd0mSalt";
+      byte[] bytes = provider.ComputeHash(Encoding.UTF32.GetBytes(salt + text));
+      return BitConverter.ToString(bytes).Replace("-", "").ToLower();
+    }
+  }
+}
